79649e7e6382d5c03c5d8ef5d4f855c941671cce
Joosep Pata <joosep.pata@cern.ch>
2014-03-12 12:58:08 +0200

    comments

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 scram project -n CMSSW CMSSW CMSSW_5_3_16_patch1
 
+#pull PU jet ID code
 git submodule add https://github.com/jpata/UserCode-CMG-CMGTools-External CMSSW/src/CMGTools/External
+
+#pull common PAT code for TTH and singletop
 git submodule add https://github.com/jpata/tth-pfbreco CMSSW/src/UserCode/TTHBBPAT
 
 #install electron ID
@@ -10,3 +13,5 @@ git cms-addpkg EgammaAnalysis/ElectronTools
 cd EgammaAnalysis/ElectronTools/data/
 cat download.url | xargs wget
 cd ../..
+
+
