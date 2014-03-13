55adde84cc7afe2db097a3c531dffa38a7807fd2
Joosep Pata <joosep.pata@cern.ch>
2014-03-13 11:43:03 +0200

    added pre-setup cleanup

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 #abort on error
 set -e
 
+rm -Rf CMSSW/src
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 scram project -n CMSSW CMSSW CMSSW_5_3_16_patch1
 
@@ -35,5 +36,7 @@ git submodule init
 #
 ##pull common PAT code for TTH and singletop
 #git submodule add https://github.com/jpata/tth-pfbreco CMSSW/src/UserCode/TTHPAT
+
+git checout CMSSW/src
 
 echo "setup succeeded"
