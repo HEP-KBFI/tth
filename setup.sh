de0d1a1023493708839b0d5b49b3f720385658d6
Joosep Pata <joosep.pata@cern.ch>
2014-03-13 11:35:14 +0200

    some printout

--- setup.sh
+++ setup.sh
@@ -20,14 +20,19 @@ git submodule init
 cd CMSSW/src
 
 #run cmsenv
+echo "running cmsenv in  "`pwd`
 eval `scramv1 runtime -sh`
 
+echo "adding PAT"
 #PAT from https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuidePATReleaseNotes52X#Add_CSCTightHaloFilter_CMSSW_5_3
 git cms-addpkg PhysicsTools/PatAlgos
 
+echo "adding ElectronTools"
 #electron ID
 git cms-addpkg EgammaAnalysis/ElectronTools
+echo "getting electron MVAs "
 cd EgammaAnalysis/ElectronTools/data/
 cat download.url | xargs wget
 cd ../..
 
+echo "setup succeeded"
