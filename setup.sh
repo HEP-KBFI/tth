8f6ba572e8d0745629e53bd0db295a8bd5f3ffc0
Joosep Pata <joosep.pata@cern.ch>
2014-03-13 11:38:01 +0200

    reorder

--- setup.sh
+++ setup.sh
@@ -5,18 +5,6 @@ set -e
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 scram project -n CMSSW CMSSW CMSSW_5_3_16_patch1
 
-git submodule init
-
-#submodules already added
-##pull PU jet ID code
-#git submodule add https://github.com/jpata/UserCode-CMG-CMGTools-External CMSSW/src/CMGTools/External
-#
-##pull generically useful analysis modules
-#git submodule add https://github.com/jpata/AnalysisModules CMSSW/src/AnalysisModules
-#
-##pull common PAT code for TTH and singletop
-#git submodule add https://github.com/jpata/tth-pfbreco CMSSW/src/UserCode/TTHPAT
-
 cd CMSSW/src
 
 #run cmsenv
@@ -34,5 +22,18 @@ echo "getting electron MVAs "
 cd EgammaAnalysis/ElectronTools/data/
 cat download.url | xargs wget
 cd ../..
+
+echo "running submodule init"
+git submodule init
+
+#submodules already added
+##pull PU jet ID code
+#git submodule add https://github.com/jpata/UserCode-CMG-CMGTools-External CMSSW/src/CMGTools/External
+#
+##pull generically useful analysis modules
+#git submodule add https://github.com/jpata/AnalysisModules CMSSW/src/AnalysisModules
+#
+##pull common PAT code for TTH and singletop
+#git submodule add https://github.com/jpata/tth-pfbreco CMSSW/src/UserCode/TTHPAT
 
 echo "setup succeeded"
