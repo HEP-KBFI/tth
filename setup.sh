797c92bbf6709ac2ea90d16dcf8dc815f3f35787
Joosep Pata <joosep.pata@cern.ch>
2014-03-13 11:15:54 +0200

    changed submodule addition

--- setup.sh
+++ setup.sh
@@ -5,14 +5,17 @@ set -e
 source /cvmfs/cms.cern.ch/cmsset_default.sh
 scram project -n CMSSW CMSSW CMSSW_5_3_16_patch1
 
-#pull PU jet ID code
-git submodule add https://github.com/jpata/UserCode-CMG-CMGTools-External CMSSW/src/CMGTools/External
+git submodule init
 
-#pull generically useful analysis modules
-git submodule add https://github.com/jpata/AnalysisModules CMSSW/src/AnalysisModules
-
-#pull common PAT code for TTH and singletop
-git submodule add https://github.com/jpata/tth-pfbreco CMSSW/src/UserCode/TTHPAT
+#submodules already added
+##pull PU jet ID code
+#git submodule add https://github.com/jpata/UserCode-CMG-CMGTools-External CMSSW/src/CMGTools/External
+#
+##pull generically useful analysis modules
+#git submodule add https://github.com/jpata/AnalysisModules CMSSW/src/AnalysisModules
+#
+##pull common PAT code for TTH and singletop
+#git submodule add https://github.com/jpata/tth-pfbreco CMSSW/src/UserCode/TTHPAT
 
 cd CMSSW/src
 
