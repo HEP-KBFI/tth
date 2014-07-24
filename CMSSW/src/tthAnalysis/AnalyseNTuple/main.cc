d8d19541a26cae644374269f9e53a10c30c6bc85
Morten Piibeleht <morten.piibeleht@cern.ch>
2014-07-24 13:46:28 +0300

    Refactor AnalyseNtuple/main.cc

--- CMSSW/src/tthAnalysis/AnalyseNTuple/main.cc
+++ CMSSW/src/tthAnalysis/AnalyseNTuple/main.cc
@@ -2,28 +2,33 @@
 #include "TFile.h"
 #include "TROOT.h"
 #include "TTree.h"
-#include "stdlib.h"
 #include <iostream>
-#include <string>  
+#include <string>
+#include <vector>
 
 using namespace std;
 
-int main ( int argc, char * argv[]) {
-	
-	//cout << "Number of args: " << argc << endl; for(int i=0; i < argc; i++) cout << i << ": `" << argv[i] << "`" << endl;
-	 
-	TFile f(argv[1]); //load root file
+int main(int argc, char * argv[])
+{
+	string filename=argv[1];
+	cout << "Processing file: " << filename << endl;
 
-	string fn=argv[1]; cout << "Processing file: " << fn << endl;
-
-	TTree* tree = (TTree*) gROOT->FindObject("tree");
+	TFile f(filename.c_str());
+	TTree* tree = (TTree*) f.GetObjectChecked("tree", "TTree");
 
 	AnalyseNtuple an(tree);
 
-        string tag[]={"tth125", "dyjets", "ttjets", "ttwjets", "ttzjets", "t_t", "t_s",
-                      "t_tw", "tbar_t", "tbar_s", "tbar_tw", "ww", "wz", "zz", "singleMu", "singleEl"};
+	vector<string> tags {
+		"tth125", "dyjets", "ttjets", "ttwjets", "ttzjets", "t_t", "t_s",
+		"t_tw", "tbar_t", "tbar_s", "tbar_tw", "ww", "wz", "zz", "singleMu",
+		"singleEl"
+	};
 
-	for (int i=0; i<16; i++){ if (fn.find(tag[i]) != std::string::npos) an.flag(i); }
+	for(int i=0; i<tags.size(); i++){
+		if(filename.find(tags[i]) != std::string::npos) {
+			an.flag(i);
+		}
+	}
 
 	an.Loop();
 
